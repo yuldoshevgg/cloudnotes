e74337812db69d479dc04b0d9032ecc1bbbdbe16
yuldoshevgg <yuldoshevgg@gmail.com>
2026-03-05 16:14:40 +0500

    added testing

--- notes/tests.py
+++ notes/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from .models import Notebook
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_create_notebook():
+    user = User.objects.create_user(username="testuser", password="pass123")
+    notebook = Notebook.objects.create(title="Test", owner=user)
+
+    assert notebook.title == "Test"
+    assert notebook.owner.username == "testuser"
